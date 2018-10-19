65b9bfd4151cb5db40857f1a134c86817f7373b6
akekeke <ke.amanda.j@gmail.com>
2018-10-19 12:33:03 +0900

    added critical TODO

--- internal/wallet/wallet.go
+++ internal/wallet/wallet.go
@@ -33,6 +33,8 @@ func NewWallet(params chaincfg.Params, seed []byte) (*Wallet, error) {
 	wallet.params = params
 	// wallet.rpc = rpc
 	wallet.size = 16
+
+	// TODO: change later, not safe for protection!!!
 	mExtKey, err := hdkeychain.NewMaster(seed, &params)
 	if err != nil {
 		log.Printf("hdkeychain.NewMaster error : %v", err)
