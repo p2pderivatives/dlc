bdaad75f33272f9402494485cd4a5d857d0db1a0
akekeke <ke.amanda.j@gmail.com>
2018-10-24 17:01:29 +0900

    refactored code to use rpcclient library

--- internal/rpc/rpc.go
+++ internal/rpc/rpc.go
@@ -2,13 +2,9 @@
 package rpc
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"time"
+
+	"github.com/btcsuite/btcd/rpcclient"
 )
 
 // BtcRPC is request info.
@@ -41,95 +37,18 @@ type Error struct {
 	Message string `json:"message"`
 }
 
-// UnmarshalError converts the error response into an RPCError type.
-func (res *Response) UnmarshalError() (*Error, error) {
-	rerr := &Error{}
-	if res.Error == nil {
-		return nil, fmt.Errorf("Rpesponse Error is nil")
+// NewBtcdRPC returns new rpcclient.Client.
+func NewBtcdRPC(url, user, pass string) (*rpcclient.Client, error) {
+	// convert url, user, pass strings into connConfig
+	connCfg := &rpcclient.ConnConfig{
+		Host:         url,
+		User:         user,
+		Pass:         pass,
+		HTTPPostMode: true, // Bitcoin core only supports HTTP POST mode
+		DisableTLS:   true, // Bitcoin core does not provide TLS by default
 	}
-	data, ok := res.Error.(map[string]interface{})
-	if !ok {
-		return nil, fmt.Errorf("RpcResponse Error is not map[string]interface{}")
-	}
-	bs, err := json.Marshal(data)
-	if err != nil {
-		return nil, err
-	}
-	err = json.Unmarshal(bs, rerr)
-	if err != nil {
-		return nil, err
-	}
-	return rerr, nil
-}
 
-// UnmarshalResult converts the response into an result type.
-func (res *Response) UnmarshalResult(result interface{}) error {
-	if res.Result == nil {
-		return fmt.Errorf("RpcResponse Result is nil")
-	}
-	var bs []byte
-	var err error
-	m, ok := res.Result.(map[string]interface{})
-	if !ok {
-		arr, ok := res.Result.([]interface{})
-		if !ok {
-			return fmt.Errorf("RpcResponse Result is neither map[string]interface{} nor []interface{}")
-		}
-		bs, err = json.Marshal(arr)
-	} else {
-		bs, err = json.Marshal(m)
-	}
-	if err != nil {
-		return err
-	}
-	err = json.Unmarshal(bs, result)
-	return err
-}
-
-// NewBtcRPC returns new BtcRPC.
-func NewBtcRPC(url, user, pass string) *BtcRPC {
-	return &BtcRPC{url, user, pass, false}
-}
-
-// Request requests server.
-func (rpc *BtcRPC) Request(method string, params ...interface{}) (*Response, error) {
-	res := &Response{}
-	if len(params) == 0 {
-		params = []interface{}{}
-	}
-	id := fmt.Sprintf("%d", time.Now().Unix())
-	req := &BtcRPCRequest{"1.0", id, method, params}
-	bs, err := json.Marshal(req)
-	if err != nil {
-		return nil, err
-	}
-	rpc.log("%s\n", bs)
-	client := &http.Client{}
-	hreq, err := http.NewRequest("POST", rpc.URL, bytes.NewBuffer(bs))
-	if err != nil {
-		return nil, err
-	}
-	hreq.SetBasicAuth(rpc.User, rpc.Pass)
-	hres, err := client.Do(hreq)
-	if err != nil {
-		return nil, err
-	}
-	defer func() {
-		err = hres.Body.Close()
-		if err != nil {
-			log.Printf("close error : %+v", err)
-		}
-	}()
-	body, err := ioutil.ReadAll(hres.Body)
-	if err != nil {
-		return nil, err
-	}
-	rpc.log("%d, %s\n", hres.StatusCode, body)
-	err = json.Unmarshal(body, res)
-	if err != nil || hres.StatusCode != http.StatusOK || res.ID != id {
-		return nil, fmt.Errorf("status:%v, error:%v, body:%s reqid:%v, resid:%v", hres.Status, err, body, id, res.ID)
-	}
-	return res, nil
+	return rpcclient.New(connCfg, nil)
 }
 
 func (rpc *BtcRPC) log(format string, v ...interface{}) {
--- internal/rpc/rpc_test.go
+++ internal/rpc/rpc_test.go
@@ -6,25 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const (
-	// RecommendedSeedLen is the recommended length in bytes for a seed
-	// to a master node.
-	RecommendedSeedLen = 32 // 256 bits
-
-)
-
-func TestNewBtcRPC(t *testing.T) {
-	rpc := NewBtcRPC("anyport", "rpcuser", "rpcpassword")
+func TestNewBtcdRPC(t *testing.T) {
+	rpc, _ := NewBtcdRPC("anyport", "rpcuser", "rpcpassword")
 	assert.NotNil(t, rpc)
-}
-
-// TODO
-func TestRequest(t *testing.T) {
-	// mock bitcoind/testnet or testnet?
-	//testy := NewBtcRPC("http://localhost:18443", "user", "pass")
-	//test_rpc := NewBtcRPC("http://localhost:18332", "akek", "akek")
-	//res2, _ := test_rpc.Request("getblockchaininfo")
-
-	//fmt.Printf("%+v\n", res)
-	//fmt.Printf("%+v\n", res2)
 }
