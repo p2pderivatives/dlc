4ddd314d08993cd087d9741795285a3da3f528f9
akekeke <ke.amanda.j@gmail.com>
2018-10-30 12:18:11 +0900

    added teardown func

--- internal/wallet/wallet_test.go
+++ internal/wallet/wallet_test.go
@@ -2,6 +2,7 @@ package wallet
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"testing"
 
@@ -9,6 +10,18 @@ import (
 	"github.com/btcsuite/btcutil/hdkeychain"
 	"github.com/btcsuite/btcwallet/waddrmgr"
 	"github.com/stretchr/testify/assert"
+)
+
+var (
+	pubPassphrase  = []byte("_DJr{fL4H0O}*-0\n:V1izc)(6BomK")
+	privPassphrase = []byte("81lUHXnOMZ@?XXd7O9xyDIWIbXX-lj")
+
+	//dirName    = "./testdb"
+	walletName = "testwallet"
+
+	params = chaincfg.RegressionNetParams
+
+	waddrmgrTestNamespaceKey = []byte("waddrmgrNamespace")
 )
 
 func TestCreateWallet(t *testing.T) {
@@ -30,49 +43,55 @@ func TestCreateWallet(t *testing.T) {
 }
 
 // TODO: create testing interface
-
-func TestCreateAccount(t *testing.T) {
-	params := chaincfg.RegressionNetParams
-	pubPass := []byte("testpub")
-	privPass := []byte("testpri")
-	dbFilePath := "./testdb2"
-	walletName := "testwallet"
+// setupManager creates a new address manager and returns a teardown function
+// that should be invoked to ensure it is closed and removed upon completion.
+func setupManager(t *testing.T) (tearDownFunc func(), wallet *Wallet) {
+	// Create a temporary directory for testing.
+	dirName, err := ioutil.TempDir("", "managertest")
+	if err != nil {
+		t.Fatalf("Failed to create db temp dir: %v", err)
+	}
 
 	seed, _ := hdkeychain.GenerateSeed(hdkeychain.RecommendedSeedLen)
 
-	wallet, _ := CreateWallet(params, seed, pubPass, privPass, dbFilePath, walletName)
-	assert.NotNil(t, wallet.Manager)
+	wallet, err = CreateWallet(params, seed, pubPassphrase, privPassphrase, dirName, walletName)
+	if err != nil {
+		wallet.db.Close()
+		_ = os.RemoveAll(dirName)
+		t.Fatalf("Failed to create test Wallet: %v", err)
+	}
+
+	tearDownFunc = func() {
+		wallet.Manager.Close()
+		wallet.db.Close()
+		_ = os.RemoveAll(dirName)
+	}
+
+	return tearDownFunc, wallet
+}
+
+func TestCreateAccount(t *testing.T) {
+	tearDownFunc, wallet := setupManager(t)
+	defer tearDownFunc()
 
 	expectedAccountNumber := uint32(1)
 
 	testAccountName := "testy"
-	account, _ := wallet.CreateAccount(waddrmgr.KeyScopeBIP0084, testAccountName, privPass)
+	account, _ := wallet.CreateAccount(waddrmgr.KeyScopeBIP0084, testAccountName, privPassphrase)
 
 	assert.Equal(t, expectedAccountNumber, account)
-
-	_ = os.RemoveAll(dbFilePath)
 }
 
 func TestNewExternalAddress(t *testing.T) {
-	params := chaincfg.RegressionNetParams
-	pubPass := []byte("testpub")
-	privPass := []byte("testpri")
-	dbFilePath := "./testdb2"
-	walletName := "testwallet"
-
-	seed, _ := hdkeychain.GenerateSeed(hdkeychain.RecommendedSeedLen)
-
-	wallet, _ := CreateWallet(params, seed, pubPass, privPass, dbFilePath, walletName)
-	assert.NotNil(t, wallet.Manager)
+	tearDownFunc, wallet := setupManager(t)
+	defer tearDownFunc()
 
 	testAccountName := "testy"
-	account, _ := wallet.CreateAccount(waddrmgr.KeyScopeBIP0084, testAccountName, privPass)
+	account, _ := wallet.CreateAccount(waddrmgr.KeyScopeBIP0084, testAccountName, privPassphrase)
 
 	numAddresses := uint32(4)
 
-	addrs, _ := wallet.NewExternalAddress(waddrmgr.KeyScopeBIP0084, privPass, account, numAddresses)
+	addrs, _ := wallet.NewExternalAddress(waddrmgr.KeyScopeBIP0084, privPassphrase, account, numAddresses)
 	fmt.Printf("%+v", addrs)
 	fmt.Printf("%+v", addrs[0])
-
-	_ = os.RemoveAll(dbFilePath)
 }
