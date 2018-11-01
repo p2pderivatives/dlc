dccb00240b3a5a1644c56f0e651217ec9ffe875f
akekeke <ke.amanda.j@gmail.com>
2018-11-01 13:49:19 +0900

    added comments and fixed lint

--- internal/wallet/utxo_test.go
+++ internal/wallet/utxo_test.go
@@ -4,6 +4,12 @@ import (
 	"testing"
 )
 
+// Test setup?
+// 		create wallet
+// 		mine regtest coins?
+//  	ListUnspent() to check if we can see the mined coins?
+// TestListUnspent() will also need to check different types of scripts
+
 func TestListUnspent(t *testing.T) {
 	// I'm not sure how to test this yet...
 	// just pray that it works lol
