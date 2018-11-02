1cbedcfb81674cee6f31a0db6cf594cf540795c4
Jwata <watanabe0621@gmail.com>
2018-11-02 18:13:16 +0900

    add test for 2-of-2 multisig script

--- internal/script/witness_test.go
+++ internal/script/witness_test.go
@@ -10,11 +10,14 @@ import (
 )
 
 func TestP2WPKHpkScript(t *testing.T) {
-	var err error
 	assert := assert.New(t)
+
 	priv, pub := test.RandKeys()
 	amt := int64(10000)
-	pkScript, _ := P2WPKHpkScript(pub)
+
+	// create P2WPKHpkScript
+	pkScript, err := P2WPKHpkScript(pub)
+	assert.Nil(err)
 
 	// prepare source tx
 	sourceTx := test.SourceTx()
@@ -27,7 +30,42 @@ func TestP2WPKHpkScript(t *testing.T) {
 	sign, err := WitnessSignature(redeemTx, 0, amt, pkScript, priv)
 	assert.Nil(err)
 
+	// redeem script
 	wt := wire.TxWitness{sign, pub.SerializeCompressed()}
+	redeemTx.TxIn[0].Witness = wt
+
+	// execute script
+	err = executeScript(pkScript, redeemTx, amt)
+	assert.Nil(err)
+}
+
+func TestMultiSigScript2of2(t *testing.T) {
+	assert := assert.New(t)
+
+	priv1, pub1 := test.RandKeys()
+	priv2, pub2 := test.RandKeys()
+	amt := int64(10000)
+
+	script, err := MultiSigScript2of2(pub1, pub2)
+	assert.Nil(err)
+	pkScript, err := P2WSHpkScript(script)
+	assert.Nil(err)
+
+	// prepare source tx
+	sourceTx := test.SourceTx()
+	sourceTx.AddTxOut(wire.NewTxOut(amt, pkScript))
+
+	// create redeem tx
+	redeemTx := createRedeemTx(sourceTx)
+
+	// witness signatures
+	sign1, err := WitnessSignature(redeemTx, 0, amt, script, priv1)
+	assert.Nil(err)
+	sign2, err := WitnessSignature(redeemTx, 0, amt, script, priv2)
+	assert.Nil(err)
+
+	// redeem script
+	wt := wire.TxWitness{[]byte{}, sign1, sign2, script}
 	redeemTx.TxIn[0].Witness = wt
 
 	// execute script
