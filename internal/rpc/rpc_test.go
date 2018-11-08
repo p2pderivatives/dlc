c29c4d7a05e597b7c86a26f8753bdedb17366459
akekeke <ke.amanda.j@gmail.com>
2018-11-08 14:08:47 +0900

    added mock

--- internal/rpc/rpc_test.go
+++ internal/rpc/rpc_test.go
@@ -4,9 +4,10 @@ import (
 	"log"
 	"testing"
 
+	"github.com/btcsuite/btcd/btcjson"
 	"github.com/dgarage/dlc/internal/mocks"
-	"github.com/dgarage/dlc/internal/wallet"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 var (
@@ -25,11 +26,11 @@ func TestNewClient(t *testing.T) {
 }
 
 func mockListUnspent(w *mocks.Wallet, err error) *mocks.Wallet {
-	utxo := wallet.Utxo{
+	utxo := btcjson.ListUnspentResult{
 		TxID:          "ce9d930c2664547ad8aba6944c8047321bde0c1c1d6551c41ebb8d9ad975dd0b",
 		Vout:          uint32(0),
 		Address:       "tb1qds49lkplvws9q4df04e5e9nq5d6asnkkhna8hg",
-		Account:       nil,
+		Account:       "",
 		ScriptPubKey:  "00146c2a5fd83f63a05055a97d734c9660a375d84ed6",
 		RedeemScript:  "",
 		Amount:        float64(0.31864472),
@@ -37,7 +38,15 @@ func mockListUnspent(w *mocks.Wallet, err error) *mocks.Wallet {
 		Spendable:     true,
 	}
 
-	w.On("ListUnspent").Return([]wallet.Utxo{utxo}, err)
+	w.On("ListUnspent").Return([]btcjson.ListUnspentResult{utxo}, err)
+
+	return w
+}
+
+func mockImportAddress(w *mocks.Wallet, address string, err error) *mocks.Wallet {
+	w.On("ImportAddress",
+		mock.AnythingOfType("string"),
+	).Return(err)
 
 	return w
 }
