854df9e093e64cf024d880d8112285e4497a9564
akekeke <ke.amanda.j@gmail.com>
2018-11-08 13:53:00 +0900

    added mock

--- internal/rpc/rpc_test.go
+++ internal/rpc/rpc_test.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"testing"
 
+	"github.com/dgarage/dlc/internal/mocks"
+	"github.com/dgarage/dlc/internal/wallet"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -20,6 +22,24 @@ func TestNewClient(t *testing.T) {
 		log.Fatal(err)
 	}
 	assert.NotNil(t, client)
+}
+
+func mockListUnspent(w *mocks.Wallet, err error) *mocks.Wallet {
+	utxo := wallet.Utxo{
+		TxID:          "ce9d930c2664547ad8aba6944c8047321bde0c1c1d6551c41ebb8d9ad975dd0b",
+		Vout:          uint32(0),
+		Address:       "tb1qds49lkplvws9q4df04e5e9nq5d6asnkkhna8hg",
+		Account:       nil,
+		ScriptPubKey:  "00146c2a5fd83f63a05055a97d734c9660a375d84ed6",
+		RedeemScript:  "",
+		Amount:        float64(0.31864472),
+		Confirmations: int64(30006),
+		Spendable:     true,
+	}
+
+	w.On("ListUnspent").Return([]wallet.Utxo{utxo}, err)
+
+	return w
 }
 
 // func setupRegNet() {
