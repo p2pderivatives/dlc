f486ccb9aecba89e07416257416665b6196a5d21
Jwata <watanabe0621@gmail.com>
2018-11-20 14:38:44 +0900

    start regtest bitcoind with initial blocks

--- scripts/run_bitcoind.sh
+++ scripts/run_bitcoind.sh
@@ -1,5 +1,24 @@
 #!/bin/bash -euC
 
 bitcoind=$(command -v bitcoind)
+bitcoincli=$(command -v bitcoin-cli)
 
-$bitcoind -datadir=./bitcoind -conf=./bitcoin.regtest.conf
+# start deamon
+echo "Starting bitcoind"
+$bitcoind -datadir=./bitcoind -conf=./bitcoin.regtest.conf &
+
+# wait until accepting rpc requests
+echo "Waiting for initilization"
+function getnetworkinfo() {
+  $bitcoincli -datadir=./bitcoind -conf=bitcoin.regtest.conf getnetworkinfo &> /dev/null
+  echo $?
+}
+while true;do
+  if [[ "$(getnetworkinfo)" == "0" ]];then
+    break
+  fi
+  sleep 0.1
+done
+
+echo "Generating initial regtest blocks"
+$bitcoincli -datadir=./bitcoind -conf=bitcoin.regtest.conf generate 101 &> /dev/null
